721ff198d59add0c6edc60f1cf8a4ae2899f9103
spope <spope851@gmail.com>
2022-10-31 07:35:53 -0600

    fix tabs bugs. add active tab style

--- client/projects/projects.js
+++ client/projects/projects.js
@@ -5,25 +5,40 @@ const secondTestBtn = document.getElementById('test-second-btn')
 const secondTestRow = document.getElementById('test-second')
 
 langFlashBtn.addEventListener('click', () => {
-    langFlashRow.style = 'display: table-row'
-    secondTestRow.style = 'display: none'
+    langFlashRow.style = 'display: table-row;'
+    secondTestRow.style = 'display: none;'
     document.location.hash = 'language-flash'
+    langFlashBtn.classList.add('active')
+    secondTestBtn.classList.remove('active')
 })
 
 secondTestBtn.addEventListener('click', () => {
-    secondTestRow.style = 'display: table-row'
-    langFlashRow.style = 'display: none'
+    secondTestRow.style = 'display: table-row;'
+    langFlashRow.style = 'display: none;'
     document.location.hash = 'test-second'
+    secondTestBtn.classList.add('active')
+    langFlashBtn.classList.remove('active')
 })
 
-const rows = document.querySelectorAll('tr')
+const rows = Array.from(document.querySelectorAll('tr')).slice(1)
 
-if (document.location.hash) {
+const hash = document.location.hash
+
+const tabs = document.querySelector('.tabs').children
+
+Array.from(tabs).map(tab => tab.classList.remove('active'))
+
+if (hash) {
+    document.getElementById(`${hash.slice(1)}-btn`).classList.add('active')
     rows.forEach(row => {
-        if (row.id === (document.location.hash.slice(1) || 'thead')) row.style = 'display: none;'
+        console.log(row.id);
+        if (row.id !== hash.slice(1)) {
+            row.style = 'display: none;'
+        }
     })
 } else {
-    Array.from(rows).slice(2).forEach(row => {
-        row.style = 'display: none;' 
+    tabs[0].classList.add('active')
+    rows.slice(1).forEach(row => {
+        row.style = 'display: none;'
     })
 }
