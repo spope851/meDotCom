72303f1de1228f81ebcfecc2b851917d96cfacff
spope <spope851@gmail.com>
2022-11-21 15:44:50 -0700

    replace build instructions with published images in compose. pull token out of server code and add to env vars

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/getTweets', async (req, res) => {
         `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=s_pop3&count=${req.query.count}&tweet_mode=extended`,
         {
         headers: {
-            Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAADj0RAEAAAAANbcQ43Jw27zlY4Hi6He4V7tVPNM%3DH0xIrAUbqSoihIZOuOpzMBatzgZ0JcgziECnosFmqIlcWr8Ycd`,
+            Authorization: `Bearer ${process.env.TWITTER_BEARER}`,
         },
         }
     ).then(data => data.json()).then(r => res.send(r))
