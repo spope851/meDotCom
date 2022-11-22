912f7d770dbb380efd8fe3d3d0344f74cd86a48d
spope <spope851@gmail.com>
2022-11-22 12:45:08 -0700

    pass secret credentials into containers from shell

--- server/keys.js
+++ server/keys.js
@@ -1,5 +1,5 @@
-export const pgUser = process.env.PGUSER;
+export const pgUser = process.env.POSTGRES_USER;
+export const pgPassword = process.env.POSTGRES_PASSWORD;
+export const pgDatabase = process.env.POSTGRES_DB;
 export const pgHost = process.env.PGHOST;
-export const pgDatabase = process.env.PGDATABASE;
-export const pgPassword = process.env.PGPASSWORD;
 export const pgPort = process.env.PGPORT;
