83b06548dfa960c89751c8e0eea61196f6ddb93f
spope <spope851@gmail.com>
2022-12-18 16:26:00 -0700

    add two-truths to projects page. twitch link. pgport standardized

--- client/projects/projects.js
+++ client/projects/projects.js
@@ -1,23 +1,40 @@
+const twoTruthsBtn = document.getElementById('two-truths-btn')
+const twoTruthsRow = document.getElementById('two-truths')
+
 const langFlashBtn = document.getElementById('language-flash-btn')
 const langFlashRow = document.getElementById('language-flash')
 
 const cubeSolverBtn = document.getElementById('cube-solver-btn')
 const cubeSolverRow = document.getElementById('cube-solver')
 
+twoTruthsBtn.addEventListener('click', () => {
+    twoTruthsRow.style = 'display: table-row;'
+    langFlashRow.style = 'display: none;'
+    cubeSolverRow.style = 'display: none;'
+    document.location.hash = 'two-truths'
+    twoTruthsBtn.classList.add('active')
+    langFlashBtn.classList.remove('active')
+    cubeSolverBtn.classList.remove('active')
+})
+
 langFlashBtn.addEventListener('click', () => {
     langFlashRow.style = 'display: table-row;'
     cubeSolverRow.style = 'display: none;'
+    twoTruthsRow.style = 'display: none;'
     document.location.hash = 'language-flash'
     langFlashBtn.classList.add('active')
     cubeSolverBtn.classList.remove('active')
+    twoTruthsBtn.classList.remove('active')
 })
 
 cubeSolverBtn.addEventListener('click', () => {
     cubeSolverRow.style = 'display: table-row;'
     langFlashRow.style = 'display: none;'
+    twoTruthsRow.style = 'display: none;'
     document.location.hash = 'cube-solver'
     cubeSolverBtn.classList.add('active')
     langFlashBtn.classList.remove('active')
+    twoTruthsBtn.classList.remove('active')
 })
 
 const rows = Array.from(document.querySelectorAll('tr')).slice(1)
